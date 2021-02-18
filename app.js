024c6481acea38e7147333514230de6be9ac3ed7
facuNorie <noriegafacundo08@gmail.com>
2021-02-18 00:04:57 -0300

    Proyecto iniciado, solo falta agregar catch

---
+++ app.js
@@ -0,0 +1,35 @@
+let btn = document.querySelector(".btnSearch");
+let input = document.querySelector(".inputSearch");
+
+input.addEventListener("keyup",e=>{
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        if(document.querySelector(".response").hasChildNodes) removeDog();
+        getDog(document.querySelector(".inputSearch").value);
+    }
+});
+
+btn.addEventListener("click",()=>{
+    if(document.querySelector(".response").hasChildNodes) removeDog();
+    getDog(document.querySelector(".inputSearch").value);
+})
+
+const getDog = breed => {
+    let Breed = breed.toLowerCase();
+    fetch(`https://dog.ceo/api/breed/${Breed}/images/random`)
+        .then(response=>response.json())
+        .then(dog=>{
+            let div = document.querySelector(".response");
+            let imgDog = document.createElement("IMG");
+            imgDog.src = dog.message;
+            imgDog.classList.add("imgDog");
+            div.appendChild(imgDog);
+        });
+    document.querySelector(".inputSearch").value = "";
+}
+
+const removeDog = () =>{
+    let parent = document.querySelector(".response");
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
